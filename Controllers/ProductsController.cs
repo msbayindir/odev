06547f633e514879b1f4272c5ef7e46726f4928f
Muhammed Sefa BAYINDIR <70607438+msbayindir@users.noreply.github.com>
2022-11-27 23:44:38 +0300

    Update ProductsController.cs

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using YGTOdev.Models;
+using Odev.Models;
 
 namespace Odev.Controllers
 {
